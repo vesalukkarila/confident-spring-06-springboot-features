26a27eadf33b76cfa97ffe02539c73f79895e678
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-26 15:10:01 +0300

    initial commit

---
+++ src/main/java/com/vesalukkarila/simple/webapp/springboot/SimpleWebappSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vesalukkarila.simple.webapp.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebappSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebappSpringbootApplication.class, args);
+	}
+
+}
