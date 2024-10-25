46e58cadb1c23b67335147d42c042b62a33494a5
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-25 08:23:24 +0300

    return to in memory list, add invoicecontroller, add dependency for xml response

--- src/main/java/com/vesalukkarila/simple/webapp/springboot/service/InvoiceService.java
+++ src/main/java/com/vesalukkarila/simple/webapp/springboot/service/InvoiceService.java
@@ -1,33 +1,25 @@
 package com.vesalukkarila.simple.webapp.springboot.service;
 
 import com.vesalukkarila.simple.webapp.springboot.model.Invoice;
+import com.vesalukkarila.simple.webapp.springboot.model.User;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
-import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
 
+    private List<Invoice> invoices = new CopyOnWriteArrayList<>();
     private final UserService userService;
     private final String cdnUrl;
-    private final JdbcTemplate jdbcTemplate;
 
-    public InvoiceService(UserService userService,
-                          @Value("${cdn.url}") String cdnUrl) {
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
         this.cdnUrl = cdnUrl;
-        this.jdbcTemplate = jdbcTemplate;
     }
 
     @PostConstruct
@@ -41,47 +33,20 @@ public class InvoiceService {
     }
 
 
-
-    /*1st SQL query
-    * row mapper maps every returned SQL row into ja Java object */
-    @Transactional
-    public List<Invoice> getInvoices() {
-        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
-        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
-            Invoice invoice = new Invoice();
-            invoice.setId(resultSet.getObject("id").toString());//UUID column result in UUID object, hence toString
-            invoice.setPdfUrl(resultSet.getString("pdf_url"));
-            invoice.setUserId(resultSet.getString("user_id"));
-            invoice.setAmount(resultSet.getInt("amount"));
-            return invoice;
-        });
+    public List<Invoice> findAll() {
+        return invoices;
     }
 
-    @Transactional
+
     public Invoice create(String userId, Integer amount) {
-        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
-        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
+        User user = userService.findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
 
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection
-                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
-                            Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, userId);  //
-            ps.setString(2, generatedPdfUrl);
-            ps.setInt(3, amount);
-            return ps;
-        }, keyHolder);
-
-        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString()
-                : null;
-
-        Invoice invoice = new Invoice();
-        invoice.setId(uuid);
-        invoice.setPdfUrl(generatedPdfUrl);
-        invoice.setAmount(amount);
-        invoice.setUserId(userId);
+        //TODO real pdf creation and storing it o network server
+        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
+        invoices.add(invoice);
         return invoice;
     }
 
