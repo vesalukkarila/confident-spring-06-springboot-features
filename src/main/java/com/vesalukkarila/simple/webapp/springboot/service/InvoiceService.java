676d74c3969b7a18c934b268314fe9605784f418
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-25 09:07:57 +0300

    configure datasource and add dependencies for H2 database, implement database queries in service layer

--- src/main/java/com/vesalukkarila/simple/webapp/springboot/service/InvoiceService.java
+++ src/main/java/com/vesalukkarila/simple/webapp/springboot/service/InvoiceService.java
@@ -5,20 +5,29 @@ import com.vesalukkarila.simple.webapp.springboot.model.User;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
 
-    private List<Invoice> invoices = new CopyOnWriteArrayList<>();
+    private JdbcTemplate jdbcTemplate;
     private final UserService userService;
     private final String cdnUrl;
 
-    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
+    public InvoiceService(UserService userService, JdbcTemplate jdbcTemplate, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.jdbcTemplate = jdbcTemplate;
         this.cdnUrl = cdnUrl;
     }
 
@@ -32,21 +41,44 @@ public class InvoiceService {
         System.out.println("Printout from invoiceService's predestroy");
     }
 
-
+    @Transactional
     public List<Invoice> findAll() {
-        return invoices;
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
+            Invoice invoice = new Invoice();
+            invoice.setId(resultSet.getObject("id").toString());
+            invoice.setPdfUrl(resultSet.getString("pdf_url"));
+            invoice.setUserId(resultSet.getString("user_id"));
+            invoice.setAmount(resultSet.getInt("amount"));
+            return invoice;
+        });
     }
 
-
+    @Transactional
     public Invoice create(String userId, Integer amount) {
-        User user = userService.findById(userId);
-        if (user == null) {
-            throw new IllegalStateException();
-        }
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
+        String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
 
-        //TODO real pdf creation and storing it o network server
-        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
-        invoices.add(invoice);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection
+                    .prepareStatement("insert into invoices (user_id, pdf_url, amount) values (?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, userId);  //
+            ps.setString(2, generatedPdfUrl);
+            ps.setInt(3, amount);
+            return ps;
+        }, keyHolder);
+
+        String uuid = !keyHolder.getKeys().isEmpty() ? ((UUID) keyHolder.getKeys().values().iterator().next()).toString()
+                : null;
+
+        Invoice invoice = new Invoice();
+        invoice.setId(uuid);
+        invoice.setPdfUrl(generatedPdfUrl);
+        invoice.setAmount(amount);
+        invoice.setUserId(userId);
         return invoice;
     }
 
