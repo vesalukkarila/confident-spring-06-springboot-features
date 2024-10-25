594fd0f9afb2ca78b3f569caf0d7c87669ec0ab0
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-25 07:45:19 +0300

    add comments

--- src/main/java/com/vesalukkarila/simple/webapp/springboot/SimpleWebappSpringbootApplication.java
+++ src/main/java/com/vesalukkarila/simple/webapp/springboot/SimpleWebappSpringbootApplication.java
@@ -3,6 +3,7 @@ package com.vesalukkarila.simple.webapp.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*by default will boot up an embedded Tomcat on port 8080.*/
 @SpringBootApplication
 public class SimpleWebappSpringbootApplication {
 
